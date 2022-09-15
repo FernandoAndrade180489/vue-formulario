92ce8d99a523c93322979a14a5fd82743647520c
Fernando Andrade <fernandow.and@gmail.com>
2022-09-15 09:52:58 -0300

    174. Máscaras de placas de veículos e rg

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -187,6 +187,42 @@
             </div>
           </div>
           <div class="mb-3 row">
+            <label class="col-3 col-form-label">Placa Veículo:</label>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                v-maska="'AAA-####'"
+                v-model="form.placaVeiculo"
+              />
+              <small class="text-muted">Formato: AAA-0000</small>
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">Placa Veículo Mercosul:</label>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                v-maska="'AAA#A##'"
+                v-model="form.placaVeiculoMercosul"
+              />
+              <small class="text-muted">Formato: AAA0A00</small>
+            </div>
+          </div>
+          <div class="mb-3 row">
+            <label class="col-3 col-form-label">RG:</label>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                v-maska="'#*-X'"
+                v-model="form.rg"
+              />
+              <small class="text-muted">Formato: Sem padrão</small>
+            </div>
+          </div>
+          <div class="mb-3 row">
             <label class="col-3 col-form-label">Data:</label>
             <div class="col">
               <input type="date" class="form-control" />
@@ -312,6 +348,15 @@
           <span>Cartão de Crédito: {{ form.cartaoDeCredito }}</span>
         </div>
         <div class="mb-3 row">
+          <span>Placa Veiculo: {{ form.placaVeiculo }}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>Placa Veiculo Mercosul: {{ form.placaVeiculoMercosul }}</span>
+        </div>
+        <div class="mb-3 row">
+          <span>RG: {{ form.rg }}</span>
+        </div>
+        <div class="mb-3 row">
           <span>Data:</span>
         </div>
         <div class="mb-3 row">
@@ -360,6 +405,9 @@ export default {
       cpf: "",
       cnpj: "",
       cartaoDeCredito: "",
+      placaVeiculo: "",
+      placaVeiculoMercosul: "",
+      rg: "",
     },
   }),
 };
