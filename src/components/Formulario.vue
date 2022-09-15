8d7d3154f4a64ab6c53ac966f3070bc4a12f0d7f
Fernando Andrade <fernandow.and@gmail.com>
2022-09-15 08:40:52 -0300

    165. Agrupando dados do formulário em um objeto

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -8,25 +8,29 @@
           <div class="mb-3 row">
             <label class="col-3 col-form-label">Nome:</label>
             <div class="col">
-              <input type="text" class="form-control" v-model="nome"/>
+              <input type="text" class="form-control" v-model="form.nome" />
             </div>
           </div>
           <div class="mb-3 row">
             <label class="col-3 col-form-label">E-mail:</label>
             <div class="col">
-              <input type="email" class="form-control" v-model="email"/>
+              <input type="email" class="form-control" v-model="form.email" />
             </div>
           </div>
           <div class="mb-3 row">
             <label class="col-3 col-form-label">Senha:</label>
             <div class="col">
-              <input type="password" class="form-control" v-model="senha"/>
+              <input
+                type="password"
+                class="form-control"
+                v-model="form.senha"
+              />
             </div>
           </div>
           <div class="mb-3 row">
             <label class="col-3 col-form-label">Idade:</label>
             <div class="col">
-              <input type="number" class="form-control" />
+              <input type="number" class="form-control" v-model="form.idade" />
             </div>
           </div>
           <div class="mb-3 row">
@@ -164,22 +168,22 @@
         <span class="fs-4">ESTADO DO OBJETO</span>
         <hr />
         <div class="mb-5 row">
-          <spam>Estado do objeto</spam>
+          <spam>{{ form }}</spam>
         </div>
 
         <span class="fs-4">SAÍDA DE DADOS</span>
         <hr />
         <div class="mb-3 row">
-          <spam>Nome: {{nome}}</spam>
+          <spam>Nome: {{ form.nome }}</spam>
         </div>
         <div class="mb-3 row">
-          <spam>E-mail: {{email}}</spam>
+          <spam>E-mail: {{ form.email }}</spam>
         </div>
         <div class="mb-3 row">
-          <spam>Senha: {{senha}}</spam>
+          <spam>Senha: {{ form.senha }}</spam>
         </div>
         <div class="mb-3 row">
-          <spam>Idade:</spam>
+          <spam>Idade: {{ form.idade }}</spam>
         </div>
         <div class="mb-3 row">
           <spam>Gênero:</spam>
@@ -229,9 +233,12 @@
 export default {
   name: "Formulario",
   data: () => ({
-    nome: '',
-    email: '',
-    senha: ''
-  })
+    form: {
+      nome: "Fernando",
+      email: "fernando@gmail.com",
+      senha: "1234456",
+      idade: "33",
+    },
+  }),
 };
 </script>
